24e688a8c47c7f6cbd9e6b7332fe6e2b052ef64a
Carlos Echeverría <55212109+echeverriadev@users.noreply.github.com>
2023-12-22 00:38:10 -0500

    feat(harry-potter): configure module federation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
+const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
+const remotes = (isServer) => {
+  const location = isServer ? "ssr" : "chunks";
+  return {
+    main: `main@http://localhost:3000/_next/static/chunks/remoteEntry.js`,
+  };
+};
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config, options) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "harryPotter",
+        filename: "static/chunks/remoteEntry.js",
+        remotes: {
+          'main': `main@http://localhost:3000/_next/static/chunks/remoteEntry.js`
+        },
+        extraOptions: {
+          exposePages: true,
+          automaticAsyncBoundary: true,
+        },
+      })
+    );
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
